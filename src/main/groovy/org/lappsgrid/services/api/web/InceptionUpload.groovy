2a4543ce4a40cde0bab0ce15c9ead7ec404a2140
Keith Suderman <suderman@cs.vassar.edu>
2019-07-27 12:59:24 -0500

    Inception upload service

---
+++ src/main/groovy/org/lappsgrid/services/api/web/InceptionUpload.groovy
@@ -0,0 +1,95 @@
+package org.lappsgrid.services.api.web
+
+import org.lappsgrid.rabbitmq.topic.PostOffice
+import org.lappsgrid.serialization.Data
+import org.lappsgrid.serialization.Serializer
+import org.lappsgrid.serialization.lif.Container
+import org.lappsgrid.services.api.util.HTML
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
+
+import javax.servlet.http.HttpServletResponse
+import java.util.zip.ZipEntry
+import java.util.zip.ZipInputStream
+import java.util.zip.ZipOutputStream
+
+import static org.lappsgrid.discriminator.Discriminators.Uri
+
+/**
+ *
+ */
+@RestController
+class InceptionUpload {
+
+    InceptionUpload() {
+    }
+
+    @GetMapping("/inception")
+    String get() {
+        return HTML.render("main", "INCEpTION") {
+            h1 'INCEpTION Upload Service'
+            p "This is the REST endpoint used by INCEpTION to upload files to the LAPPS/Galaxy instance."
+            p "Useful help information will be provided shortly."
+        }
+    }
+
+    @PostMapping(path="/inception", produces = "text/plain")
+    String acceptZipFile(@RequestParam MultipartFile file, @RequestParam String user, HttpServletResponse response) {
+        PostOffice po = new PostOffice("galaxy.upload.service")
+
+        ByteArrayInputStream input = new ByteArrayInputStream(file.bytes)
+        ByteArrayOutputStream output = new ByteArrayOutputStream()
+
+        ZipInputStream zin = new ZipInputStream(input)
+        ZipOutputStream zout = new ZipOutputStream(output)
+
+        ZipEntry entry = zin.getNextEntry()
+        while (entry != null) {
+            if (entry.name.startsWith("annotation/")) {
+                File f = new File(entry.name)
+                if (f.parentFile) {
+                    String id = f.parentFile.name.replace(".json", "")
+                    //println "Adding $id to zip file."
+                    String filename = "${id}.lif"
+                    String username = f.name.replace(".lif", "")
+                    String json = read(zin)
+                    json = addID(json, id)
+                    ZipEntry newEntry = new ZipEntry("$user/$username/$filename")
+                    zout.putNextEntry(newEntry)
+                    zout.write(json.bytes)
+                    zout.closeEntry()
+                }
+            }
+            entry = zin.getNextEntry()
+        }
+        zout.close()
+        po.send("zip", output.toByteArray())
+        po.close()
+        response.setStatus(HttpStatus.CREATED.value())
+        return "OK"
+    }
+
+    String addID(String json, String id) {
+        Container container = Serializer.parse(json, Container)
+        if (container.metadata.id == null) {
+            container.metadata.id = id
+        }
+        return new Data(Uri.LIF, container).asJson()
+    }
+
+    String read(ZipInputStream zip) {
+        ByteArrayOutputStream out = new ByteArrayOutputStream()
+        byte[] buffer = new byte[4096]
+        int len = zip.read(buffer)
+        while (len > 0) {
+            out.write(buffer, 0, len)
+            len = zip.read(buffer)
+        }
+        return new String(out.toByteArray(), "UTF-8")
+    }
+
+}
